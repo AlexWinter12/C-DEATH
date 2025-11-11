3fe476398d20bb4237d1764f6889f93e0fe65bfe
HONOR <rybaulin2004@gmail.com>
2025-11-11 20:46:06 +0300

    test

---
+++ C-DEATH/C-DEATH/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace  C_DEATH.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
