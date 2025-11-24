50e8012955287a0af3484608e0a90b923b366bd8
HONOR <rybaulin2004@gmail.com>
2025-11-24 21:08:29 +0300

    Created folders with methods and tests + odd ones

---
+++ C-DEATH/Methods/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+namespace OddNumbersFilter
+
+{
+    public class OddNumbers
+    {
+        public IEnumerable<int> GetOdd(ICollection<int> nums)
+        {
+            if (nums == null)
+            {
+                return Enumerable.Empty<int>();
+            }
+            return nums.Where(n => n % 2 != 0);
+        }
+    }
+}
+
+namespace PositiveNumbersFilter
+{
+    public class PositiveNumbers
+    {
+        public IEnumerable<int> GetPositiveNumbers(IEnumerable<int> numbers)
+        {
+            if (numbers == null)
+            {
+                return Enumerable.Empty<int>();
+            }
+            return numbers.Where(n => n > 0);
+        }
+    }
+}
---
+++ C-DEATH/UnitTests/CollectionsFilterTest/UnitTest1.cs
@@ -0,0 +1,69 @@
+﻿using OddNumbersFilter;
+
+public class OddNumbersTest
+{
+    //Обычный набор
+    [Fact]
+    public void 
+        CheckRegularSet()
+    {
+        // Arrange
+        var nums = new[] { 1, 2, 3, 4, 5 };
+        var filter = new OddNumbers();
+        var expected = new[] { 1, 3, 5 };
+        
+        // Act
+        var result = filter.GetOdd(nums).ToList();
+        
+        // Assert
+        Assert.Equal(expected, result);
+    }
+    
+    //Набор с отрицательными 
+    [Fact]
+    public void CheckRegular_set_Negative()
+    
+    {   // Arrange
+        var nums = new[] { 1, 2, -4, -5, 6 };
+        var filter = new OddNumbers();
+        var expected = new[] { 1, -5 };
+        
+        //Act
+        var result = filter.GetOdd(nums).ToList();
+        
+        // Assert
+        Assert.Equal(expected, result);
+    }
+    
+    //Только отрицательные
+    [Fact]
+    public void CheckNegativeSet()
+    {
+        //Arrange
+        var nums = new[] {-3,-7,-9};
+        var filter = new OddNumbers();
+        var expected = new[] { -3, -7, -9 };
+        
+        //Act 
+        var result = filter.GetOdd(nums).ToList();
+        
+        // Assert 
+        Assert.Equal(expected, result);
+    }
+
+    //Null
+    [Fact]
+    public void CheckNull()
+    {
+        //Arrange
+        var filter = new OddNumbers();
+        
+        // Act
+        var result = filter.GetOdd(null);
+        
+        // Assert
+        Assert.Empty(result);
+
+    }
+
+}
